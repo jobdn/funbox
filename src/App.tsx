afc4f3f12e5c686a77678c66f84c3b5e8823857d
Danila <peace_are_off@bk.ru>
2022-06-18 14:16:02 +0300

    feat: add background for entire app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+import { Home } from "./components/Home";
+import mainBg from "./assets/main-bg.png";
+
+const StyledApp = styled.div`
+  background: url(${mainBg}) center/cover no-repeat;
+  height: 100vh;
+  width: 100%;
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <Home />
+    </StyledApp>
   );
-}
+};
 
 export default App;
